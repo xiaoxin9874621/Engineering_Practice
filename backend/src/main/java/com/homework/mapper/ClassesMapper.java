b7cec2cfcbf0e60633d0e24fd4c7b3d62d1b0655
xiaoxin9874621 <130519924+xiaoxin9874621@users.noreply.github.com>
2026-06-22 16:15:19 +0800

    Add backend/src/main/java/com/homework/mapper/ClassesMapper.java

---
+++ backend/src/main/java/com/homework/mapper/ClassesMapper.java
@@ -0,0 +1,9 @@
+package com.homework.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.homework.entity.Classes;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ClassesMapper extends BaseMapper<Classes> {
+}
