f4be942c5d45b53833ba9b60e2c132ecdf6874ef
xiaoxin9874621 <130519924+xiaoxin9874621@users.noreply.github.com>
2026-06-22 16:15:04 +0800

    Add backend/src/main/java/com/homework/mapper/AnnotationMapper.java

---
+++ backend/src/main/java/com/homework/mapper/AnnotationMapper.java
@@ -0,0 +1,9 @@
+package com.homework.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.homework.entity.Annotation;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AnnotationMapper extends BaseMapper<Annotation> {
+}
